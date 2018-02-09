b0554bedeadd5c0f30f944810316143d29e172b6
Vadim Korabelnik <vad@oblako9.ru>
2018-02-09 21:33:55 +0300

    Added lots list

--- index.php
+++ index.php
@@ -99,22 +99,25 @@ $items = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
+            <? foreach ($items as $item => $value) :?>
+                <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=$value['img_url'] ?>" width="350" height="260" alt="<?=$value['img_alt']?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$value['category_name'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$value['name'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$value['price'] ?><b class="rub">р</b></span>
                         </div>
-                        <div class="lot__timer timer">00:54:12</div>
+                        <div class="lot__timer timer">
+                            <?=$lot_time_remaining;?>
+                        </div>
                     </div>
                 </div>
-            </li>
+                </li><? endforeach; ?>
         </ul>
     </section>
 </main>
