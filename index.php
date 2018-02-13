3527aef27561a429dbcf97b80f1e2fb065d23d9f
Vadim Korabelnik <vad@oblako9.ru>
2018-02-13 17:59:13 +0300

    More fixes

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ $categories = ['boards' => 'Доски и лыжи', 'attachment' => 'Крепл
     'clothing' => 'Одежда', 'tools' => 'Инструменты', 'other' => 'Разное'];
 
 $lots = [
-    ['name' => '2014 Rossignol District Snowboard', 'category_name' => 'Доски и лыжи', 'price' => 10999, 'img_url' => 'img/lot-1.jpg', 'img_alt' => 'Сноуборд'],
+    ['name' => '2014 Rossignol District Snowboard', 'category_name' => 'Доски и лыжи',
+        'price' => 10999, 'img_url' => 'img/lot-1.jpg', 'img_alt' => 'Сноуборд'],
 
     ['name' => 'DC Ply Mens 2016/2017 Snowboard', 'category_name' => 'Доски и лыжи',
         'price' => 159999, 'img_url' => 'img/lot-2.jpg', 'img_alt' => 'Сноуборд'],
@@ -127,25 +128,11 @@ $lots = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <? foreach ($items as $item => $item) : ?>
-                <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="<?= $item['img_url'] ?>" width="350" height="260" alt="<?= $item['img_alt'] ?>">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category"><?= $item['category_name'] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $item['name'] ?></a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $item['price'] ?><b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            <!--                            --><? //=$lot_time_remaining;?>
-                        </div>
-                    </div>
-                </div>
-                </li><? endforeach; ?>
+            <?php foreach ($categories as $category) : ?>
+                <li class="nav__item">
+                    <a href="all-lots.html"><?= $category ?></a>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
