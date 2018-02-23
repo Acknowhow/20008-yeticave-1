83cfcfb552e5979b582b828c17f9565f6b7b76fa
Vadim Korabelnik <vad@oblako9.ru>
2018-02-24 01:21:23 +0300

    Prints without modularity

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
 <?php
+require 'data/data.php';
+require 'functions.php';
 require 'defaults/config.php';
 require 'defaults/var.php';
 
-require 'data/data.php';
-require 'functions.php';
-require 'lot.php';
+require ('lot.php');
 
-if ($index === true) {
-    $content = include_template('templates/index.php',
+$index_content = '';
+
+
+$index_content = include_template('templates/index.php',
         [
             'categories' => $categories,
             'lots' => $lots, 'time_left' => $time_left
         ]
-    );
-}
+);
 
+var_dump($content);
+
+if (!empty($content)) {
+    $content = ob_get_contents();
+    $index_content = $content;
+}
 print include_template('templates/layout.php',
     [
         'is_auth' => $is_auth, 'index' => $index,
         'title' => $title, 'user_name' => $user_name,
-        'user_avatar' => $user_avatar, 'content' => $content, 'categories' => $categories
+        'user_avatar' => $user_avatar, 'content' => $index_content, 'categories' => $categories
     ]
 );
+
--- lot.php
+++ lot.php
@@ -1,22 +1,26 @@
 <?php
-// Some dynamic logic goes here
+require_once 'data/data.php';
+require_once 'functions.php';
+require_once 'defaults/config.php';
+require 'defaults/var.php';
+//
+//$error = false;
+$content = '';
 $lot_id = isset($_GET['lot-id']) ? $_GET['lot-id'] : null;
+//
+$lot = [];
 
 
-//if (isset($lot_($lot_id)) {
-//}
+// in the end send different flags according to content variable
+// – if lot-content is true, print lot-content
+// – if bet-content is true, print bet-content etc....
 
+if (isset($lot_id)) {
+    var_dump($lot_id);
 
-    if (!isset($lots[$lot_id])) {
-        http_response_code(404);
-        $content = include_template('templates/404.php',
-            [
-                'container' => $container
-            ]);
-    } else {
+    if(array_key_exists($lot_id, $lots) === true) {
         $lot = $lots[$lot_id];
         $content = include_template('templates/lot.php',
-
             [
                 'categories' => $categories, 'bets' => $bets,
                 'lot_name' => $lot['lot_name'], 'lot_category' => $lot['lot_category'],
@@ -24,6 +28,18 @@ $lot_id = isset($_GET['lot-id']) ? $_GET['lot-id'] : null;
                 'lot_value' => $lot['lot_value'], 'lot_img_url' => $lot['lot_img_url'],
                 'lot_img_alt' => $lot['lot_img_alt'], 'lot_description' => $lot['lot_description']
             ]);
-
+        print include_template('templates/layout.php',
+            [
+                'is_auth' => $is_auth, 'index' => $index,
+                'title' => $title, 'user_name' => $user_name,
+                'user_avatar' => $user_avatar, 'content' => $content, 'categories' => $categories
+            ]
+        );
     }
+    else {
+        $error = true;
+    }
+}
+
+
 
