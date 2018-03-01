5baf1032f5571bd5243e8d81af918523954240b0
Vadim Korabelnik <vad@oblako9.ru>
2018-03-01 03:52:37 +0300

    Adding login form

--- login.php
+++ login.php
@@ -10,6 +10,9 @@ require 'functions.php';
 
 require 'markup/markup.php';
 
+$user_data = [];
+$user_errors = [];
+
 $required = [
     'user_email', 'user_password'
 ];
@@ -18,18 +21,36 @@ $rules = [
     'user_email' => 'validateEmail', 'user_password' => 'validatePassword'
 ];
 
+$email_check = '';
+$password_check = '';
+
+
 if (isset($_POST['login'])) {
     foreach ($_POST as $key => $value) {
         if (in_array($key, $required) && ($value === '')) {
             $login_errors[$key] = $user_login_errors[$key]['error_message'];
+
+            $login_defaults[$key]['input'] = $value;
+        } elseif ($key === 'user_email' && $value !== '') {
+            $email_check = call_user_func($rules[$key], $value);
+        } elseif ($key === 'user_password' && $value !== '') {
+            $password_check = call_user_func(
+                'validateUser', $_POST['user_email'],
+                $users, $_POST['user_password']);
         }
 
-        if (array_key_exists($key, $rules) && $value !== '') {
-            $result = call_user_func($rules[$key], $value);
+        if (!empty($email_check)) {
+            $login_errors['user_email'] = $email_check;
+        }
 
-            if (!empty($result)) {
-                $login_errors[$key] = $result;
-            }
-        } $login_defaults[$key]['input'] = $value;
+        if (is_string($password_check)) {
+            $login_errors['user_password'] = $password_check;
+        }
+
+        if (is_array($password_check)) {
+            $user_data = $password_check;
+        }
+
+
     }
 }
