415f291488ccfb1f2b3229c6a8e1a7098b6d7f1d
Vadim Korabelnik <vad@oblako9.ru>
2018-03-01 07:32:12 +0300

    Push

--- templates/lot.php
+++ templates/lot.php
@@ -8,8 +8,8 @@
             <p class="lot-item__category">Категория: <span><?= $lot_category ?></span></p>
             <p class="lot-item__description"><?= $lot_description ?></p>
         </div>
-        <div class="lot-item__right">
-            <div class="lot-item__state <?php if (empty($is_auth)) : ?>style='display: none;'<?php endif;?>">
+        <div class="lot-item__right"><?php if ($is_auth === true) : ?>
+            <div class="lot-item__state">
                 <div class="lot-item__timer timer">
                     10:54:12
                 </div>
@@ -29,7 +29,7 @@
                     </p>
                     <button type="submit" class="button">Сделать ставку</button>
                 </form>
-            </div>
+            </div><?php endif;?>
             <div class="history">
                 <h3>История ставок (<span>4</span>)</h3>
                 <table class="history__list">
