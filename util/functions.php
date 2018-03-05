0314de55c3399af9d31cd188adbf75554e030724
Vadim Korabelnik <vad@oblako9.ru>
2018-03-05 16:50:35 +0300

    Give it a try with MySQL date functions

--- util/functions.php
+++ util/functions.php
@@ -48,6 +48,24 @@ function getDateFormat($date, $format = 'd.m.Y')
     return $_date;
 }
 
+function convertTimeStamp($timeStamp)
+{
+    // Elapsed timestamp
+    $timeLapseStamp = strtotime('now') - $timeStamp;
+    // Elapsed time in hours
+    $timeLapseHours = round($timeLapseStamp / 3600, 2);
+
+    if ($timeLapseHours < 1) {
+        return date('i минут назад', $timeStamp);
+
+    } else if ($timeLapseHours > 24) {
+        return date('d-m-y в H:i', $timeStamp);
+
+    } else {
+        return date('H часов назад', $timeStamp);
+    }
+}
+
 function validateDate($date)
 {
     $now = strtotime('now');
