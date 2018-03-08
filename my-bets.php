07026b9d4efc454d2f8e0054af806ce1dafe59d7
Vadim Korabelnik <vad@oblako9.ru>
2018-03-09 01:32:47 +0300

    Adding complex query for my bets

--- my-bets.php
+++ my-bets.php
@@ -21,25 +21,27 @@ $lot_value = $_POST['lot_value'] ?? null;
 $lot_step = $_POST['lot_step'] ?? null;
 $lot_min = $lot_value + $lot_step;
 
-if (!isset($bet_value)) {
-    $index = false;
-    $title = 'Мои ставки';
-    $content = include_template('templates/my-bets.php',
-        [
-            'lots' => $lots
-        ]
-    );
-    $markup = new Markup(
-        'templates/layout.php', array_merge_recursive(
-            $layout,
-            [
-                'index' => $index, 'title' => $title,
-                'nav' => $nav, 'content' => $content
-            ]
-        )
-    );
-    $markup->get_layout();
-}
+//if (!isset($bet_value)) {
+//    $index = false;
+//    $title = 'Мои ставки';
+//    $my_bets = select_data_assoc($link, $my_bets_sql, [$user_id]);
+//
+//    $content = include_template('templates/my-bets.php',
+//        [
+//            'my_bets' => $my_bets
+//        ]
+//    );
+//    $markup = new Markup(
+//        'templates/layout.php', array_merge_recursive(
+//            $layout,
+//            [
+//                'index' => $index, 'title' => $title,
+//                'nav' => $nav, 'content' => $content
+//            ]
+//        )
+//    );
+//    $markup->get_layout();
+//}
 
 $validate = validateBetValue($bet_value, $lot_min);
 
--- templates/my-bets.php
+++ templates/my-bets.php
@@ -1,19 +1,19 @@
 <section class="rates container">
   <h2>Мои ставки</h2>
   <table class="rates__list"><?php foreach ($my_bets as $key => $value) : ?>
-      <tr class="rates__item">
+    <tr class="rates__item">
       <td class="rates__info">
         <div class="rates__img">
-          <img src="<?=$lots[$key]['url']; ?>" width="54" height="40" alt="<?=$lots[$key]['alt']; ?>">
+          <img src="<?= $value['lot_img_url']; ?>" width="54" height="40" alt="<?= $value['category_name'] ?>">
         </div>
-        <h3 class="rates__title"><a href="lot.html"><?=$lots[$key]['name']; ?></a></h3>
+        <h3 class="rates__title"><a href="lot.php?lot_id=<?= $lot['lot_id'] ?>"><?= $value['lot_name'] ?></a></h3>
       </td>
-      <td class="rates__category"><?=$lots[$key]['category']; ?>
+      <td class="rates__category"><?=$value['category_name']; ?>
       </td>
       <td class="rates__timer">
         <div class="timer timer--finishing">07:13:34</div>
       </td>
-      <td class="rates__price"><?=$value['bet']; ?>
+      <td class="rates__price"><?=$value['bet_value']; ?>
       </td>
       <td class="rates__time"><?print(convertTimeStamp($value['date_add'])); ?>
       </td>
