415f477debd0b866e5afd073e2be9b91356afd8c
Vadim Korabelnik <vad@oblako9.ru>
2018-03-09 04:50:32 +0300

    Added query for my-bets

--- data/data.php
+++ data/data.php
@@ -15,40 +15,48 @@ $lot_description_default = '
     посмотрите на Вашу доску и улыбнитесь, крутая графика 
     от Шона Кливера еще никого не оставляла равнодушным.';
 
-$categories_eng = [
-    'boards', 'attachment', 'boots',
-    'clothing', 'tools', 'other'
-];
+$categories_eng =
+    [
+        'boards', 'attachment', 'boots',
+        'clothing', 'tools', 'other'
+    ];
 
 $categories_sql = 'SELECT * FROM categories 
 ORDER BY category_id ASC';
-$categories_fetched = select_data_column(
-    $link, $categories_sql, [], 'category_name');
+
+$categories_fetched = select_data_column($link, $categories_sql,
+    [], 'category_name');
 
 $categories = array_combine($categories_eng, $categories_fetched);
 
-$lots_sql = 'SELECT l.lot_id,l.lot_name,l.lot_date_add,
-l.lot_date_end,l.lot_description,l.lot_img_url,l.lot_value,
-l.lot_step,l.user_id,l.category_id,c.category_name 
-AS lot_category FROM lots l JOIN categories c 
-ON l.category_id=c.category_id ORDER BY l.lot_date_add DESC';
+$lots_sql = 'SELECT l.lot_id,l.lot_name,
+l.lot_date_add,l.lot_date_end,
+l.lot_description,l.lot_img_url,
+l.lot_value,l.lot_step,
+l.user_id,l.category_id,c.category_name 
+AS lot_category FROM lots l
+JOIN categories c ON l.category_id=c.category_id 
+ORDER BY l.lot_date_add DESC';
 
 $lots = select_data_assoc($link, $lots_sql, []);
 
 // Selecting all bets for the current lot by lot_id
-$bets_sql = "SELECT b.bet_id,b.lot_id,b.bet_value, 
-UNIX_TIMESTAMP (b.bet_date_add),
+$bets_sql = "SELECT b.bet_id,b.lot_id,
+b.bet_value, UNIX_TIMESTAMP(b.bet_date_add),
 b.user_id,u.user_name AS bet_author 
 FROM bets b JOIN users u ON b.user_id=u.user_id 
-WHERE b.lot_id=? ORDER BY b.bet_date_add DESC LIMIT $bet_display_count";
+WHERE b.lot_id=? ORDER BY b.bet_date_add 
+DESC LIMIT $bet_display_count";
 
-$my_bets_sql = 'SELECT * FROM bets b JOIN
-(SELECT l.lot_id,l.lot_name,UNIX_TIMESTAMP (l.lot_date_end),
-l.lot_description,l.lot_img_url,l.user_id,l.category_id FROM lots l) x
-ON x.lot_id = b.lot_id JOIN categories c ON x.category_id=c.category_id
-JOIN users u ON x.user_id=u.user_id AND b.user_id=?';
+// Query for my bets
+$my_bets_sql = 'SELECT b.bet_value, UNIX_TIMESTAMP(b.bet_date_add),
+b.user_id,b.bet_is_win,l.lot_name,UNIX_TIMESTAMP(l.lot_date_end),
+c.category_name,l.lot_img_url 
+FROM bets b JOIN (lots l JOIN categories c ON l.category_id=c.category_id) 
+ON l.lot_id = b.lot_id AND b.user_id=?';
 
-$layout = [
-    'is_auth' => $is_auth,
-    'user' => $user, 'categories' => $categories
-];
+$layout =
+    [
+        'is_auth' => $is_auth,
+        'user' => $user, 'categories' => $categories
+    ];
--- templates/my-bets.php
+++ templates/my-bets.php
@@ -4,19 +4,18 @@
     <tr class="rates__item">
       <td class="rates__info">
         <div class="rates__img">
-          <img src="<?= $value['lot_img_url']; ?>" width="54" height="40" alt="<?= $value['category_name'] ?>">
+          <img src="<?= $value['lot_img_url']; ?>" width="54" height="40" alt="<?= $value['lot_category'] ?>">
         </div>
-        <h3 class="rates__title"><a href="lot.php?lot_id=<?= $lot['lot_id'] ?>"><?= $value['lot_name'] ?></a></h3>
+        <h3 class="rates__title"><a href="lot.php?lot_id=<?= $value['lot_id'] ?>"><?= $value['lot_name'] ?></a></h3>
       </td>
-      <td class="rates__category"><?=$value['category_name']; ?>
+      <td class="rates__category"><?=$value['lot_category']; ?>
       </td>
       <td class="rates__timer">
-        <div class="timer timer--finishing">07:13:34</div>
+        <div class="timer timer--finishing"></div>
       </td>
       <td class="rates__price"><?=$value['bet_value']; ?>
       </td>
-      <td class="rates__time"><?print(convertTimeStamp($value['lot_date_end'])); ?>
-      </td>
+      <td class="rates__time"><?= convertTimeStamp($value['UNIX_TIMESTAMP(b.bet_date_add)']) ?></td>
       </tr><?php endforeach; ?>
   </table>
 </section>
