9db3f596d7b22850fa1d42dc5022a5b19a2b37e9
Vadim Korabelnik <vad@oblako9.ru>
2018-05-31 18:00:37 +0300

    Added author rights

--- templates/lot.php
+++ templates/lot.php
@@ -10,7 +10,8 @@
             <p class="lot-item__category">Категория: <span><?= $lot_category ?></span></p>
             <p class="lot-item__description"><?= htmlspecialchars($lot_description) ?></p>
         </div>
-        <div class="lot-item__right"><?php if ($is_auth === true) : ?>
+        <div class="lot-item__right">
+<?php if ($is_auth === true) : ?>
             <div class="lot-item__state">
                 <div class="lot-item__timer timer">
                     10:54:12
@@ -25,7 +26,7 @@
                     </div>
                 </div>
                 <form class="lot-item__form" action="my-bets.php" method="post">
-                    <?php if (isset($bet_author)) : ?><!-- fixing button position -->
+<?php if (isset($bet_author)) : ?><!-- fixing button position -->
                         <style>
                             .lot-item__form {
                                 position: relative
@@ -38,7 +39,9 @@
                             .form__error {
                                 width: 130px;
                             }
-                        </style><?php endif; ?>
+                        </style>
+<?php endif; ?>
+<?php if ($my_lot === false && $bet_author === false) : ?>
                     <p class="lot-item__form-item<?php if (!empty($bet_error)) : ?> form__item--invalid<?php endif; ?>">
                         <label for="bet_value">Ваша ставка</label>
                         <input <?php if($bet_author === true) : ?>disabled<?php endif; ?>
@@ -50,23 +53,24 @@
                         <input type="hidden" name="lot_step" value="<?= htmlspecialchars($lot_step) ?>">
                         <span class="form__error"><?php if (!empty($bet_error)) : ?><?= $bet_error ?><?php endif; ?></span>
                     </p>
-                    <?php if ($my_lot === false && $bet_author === false) : ?><button type="submit" class="button">Сделать ставку</button><?php endif;?>
+                    <button type="submit" class="button">Сделать ставку</button>
+<?php endif;?>
                 </form>
-            </div><?php endif; ?>
+            </div>
+<?php endif; ?>
             <div class="history">
-            <?php if(!empty($bets)) : ?>
+        <?php if(!empty($bets)) : ?>
                 <h3>История ставок (<span><?= count($bets); ?></span>)</h3>
                 <table class="history__list">
-
-                    <?php foreach ($bets as $bet) : ?>
+            <?php foreach ($bets as $bet) : ?>
                         <tr class="history__item">
                             <td class="history__name"><?= $bet['bet_author'] ?></td>
                             <td class="history__price"><?= $bet['value'] ?> р</td>
                             <td class="history__time"><?= convertBetTimeStamp($bet['UNIX_TIMESTAMP(b.date_add)']) ?></td>
                         </tr>
-                    <?php endforeach; ?><?php endif; ?>
-
+            <?php endforeach; ?>
                 </table>
+        <?php endif; ?>
             </div>
         </div>
     </div>
